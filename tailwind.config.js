77372b3458f2ad64901a44bda8084287db2d15bd
svjeto <julianferrari.rda@gmail.com>
2025-08-07 14:42:09 +0000

    recuperacion diseño

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./js/**/*.js", // O el patrón que corresponda a tus archivos JavaScript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
